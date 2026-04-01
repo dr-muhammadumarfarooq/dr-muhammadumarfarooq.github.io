546c85221686ec612397a88ae383af1700c73a53
dr-muf <100495659+dr-muf@users.noreply.github.com>
2026-04-01 17:12:33 +0000

    Deploying to gh-pages from @ dr-muf/dr-muf.github.io@d470762b1ab310c2ce991aa21e98d377ac26a5e3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Iammufarooq/mufarooq.github.io",
+    "data-repo": "dr-muf/dr-muf.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
